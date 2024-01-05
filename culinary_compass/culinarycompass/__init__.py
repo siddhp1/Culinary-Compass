ebd3317bfe51908a6974d1df577dc67a6b8ed884
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-01-04 19:36:30 -0500

    added account info, password reset, basic styles

--- culinary_compass/culinarycompass/__init__.py
+++ culinary_compass/culinarycompass/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 # App and database configuration
 app = Flask(__name__)
@@ -12,6 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('DEV_EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('DEV_EMAIL_PASS')
+mail = Mail(app)
 
 # Importing after initialization of app to prevent circular import
 from culinarycompass import routes
--- culinary_compass/culinarycompass/forms.py
+++ culinary_compass/culinarycompass/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from culinarycompass.models import User
 
+# User registration form
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -23,9 +26,61 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('Email already in use. Please use a different email.')
 
+# User login form
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+# Update account form
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:         
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already in use. Please choose a different username.')
+    
+    def validate_email(self, email):       
+        if email.data != current_user.email: 
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already in use. Please use a different email.')
+
+# Questionnaire form
+
+
+# Request password reset form
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):        
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('Account with email does not exist.')
+
+# Reset password form
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+# Add restaurants form
+class AddRestaurantForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    
+    date = DateField('Date', validators=[DataRequired()])
+    
+    # This will be dictated by the maps API
+    # What information is required to find and validate that the restaurant exists

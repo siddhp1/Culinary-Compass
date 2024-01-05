f6f076555dcf5dbdbf13bd480bb307c3692a1606
Siddh Patel <47033295+siddhp123@users.noreply.github.com>
2024-01-04 19:36:30 -0500

    added account info, password reset, basic styles

--- culinary_compass/culinarycompass/models.py
+++ culinary_compass/culinarycompass/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from culinarycompass import db, login_manager
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from culinarycompass import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -14,6 +15,19 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     restaurants = db.relationship('Restaurant', backref='user', lazy=True)
+
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return(s.dumps({'user_id': self.id}))
+    
+    @staticmethod
+    def verify_reset_token(token, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, max_age=expires_sec)['user_id']
+        except:
+            return None
+        return(User.query.get(user_id))
 
     def __repr__(self):
         return(f"User('{self.username}', '{self.email}', '{self.image_file}')")

344381e9d7acac55102afb4f30cb51fcd218ad49
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-01-09 12:39:02 -0500

    bug fixes and comments

--- culinary_compass/culinarycompass/__init__.py
+++ culinary_compass/culinarycompass/__init__.py
@@ -7,19 +7,31 @@ from flask_mail import Mail
 
 # App and database configuration
 app = Flask(__name__)
+
+# Set a secret key for securing session data
 app.config['SECRET_KEY'] = '0321e05b9aff462378618d5d21b91117'
+
+# Configure the database URI for SQLAlchemy (using SQLite in this case)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Initialize SQLAlchemy for database management
 db = SQLAlchemy(app)
+
+# Initialize Bcrypt for password hashing
 bcrypt = Bcrypt(app)
+
+# Initialize LoginManager for managing user sessions
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+# Configure mail settings for sending password reset emails
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('DEV_EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('DEV_EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ.get('DEV_EMAIL_USER')  # Use environment variables for security
+app.config['MAIL_PASSWORD'] = os.environ.get('DEV_EMAIL_PASS')  # Use environment variables for security
 mail = Mail(app)
 
-# Importing after initialization of app to prevent circular import
+# Import routes after initialization of the app to prevent circular import
 from culinarycompass import routes
--- culinary_compass/culinarycompass/models.py
+++ culinary_compass/culinarycompass/models.py
@@ -3,64 +3,65 @@ from itsdangerous import URLSafeTimedSerializer as Serializer
 from culinarycompass import db, login_manager, app
 from flask_login import UserMixin
 
+# Load user callback for Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 # Models
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    dietary_preferences_id = db.Column(db.Integer, db.ForeignKey('dietary_preferences.id'), nullable=False)
-    dietary_preferences = db.relationship('DietaryPreferences', backref='user', uselist=False)
-    restaurant_visits = db.relationship('RestaurantVisit', backref='user', lazy=True)
+    # User model representing user data
+    id = db.Column(db.Integer, primary_key=True)  # User ID, primary key
+    username = db.Column(db.String(20), unique=True, nullable=False)  # User's username
+    email = db.Column(db.String(120), unique=True, nullable=False)  # User's email
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')  # Profile image file
+    password = db.Column(db.String(60), nullable=False)  # User's hashed password
+    dietary_preference = db.Column(db.String(20), default='neither')  # User's dietary preference
+    gluten = db.Column(db.Boolean, default=False)  # User's gluten preference
+    allergies = db.Column(db.Boolean, default=False)  # User's allergies preference
+    alcohol = db.Column(db.Boolean, default=False)  # User's alcohol preference
+    restaurant_visits = db.relationship('RestaurantVisit', backref='user', lazy=True)  # User's restaurant visits
 
     def get_reset_token(self):
+        # Generates a reset token for password reset
         s = Serializer(app.config['SECRET_KEY'])
-        return(s.dumps({'user_id': self.id}))
-    
+        return s.dumps({'user_id': self.id})
+
     @staticmethod
     def verify_reset_token(token, expires_sec=1800):
+        # Verifies the reset token and returns the associated user
         s = Serializer(app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token, max_age=expires_sec)['user_id']
         except:
             return None
-        return(User.query.get(user_id))
+        return User.query.get(user_id)
 
     def __repr__(self):
-        return(f"User('{self.username}', '{self.email}', '{self.image_file}')")
-    
-class DietaryPreferences(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
-    dietary_preference = db.Column(db.String(20), default='Neither')
-    gluten = db.Column(db.Boolean, default=False)
-    allergies = db.Column(db.Boolean, default=False)
-    alcohol = db.Column(db.Boolean, default=False)
-    user = db.relationship('User', backref='dietary_preferences', uselist=False)
-    
+        # String representation of the User object
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 class Restaurant(db.Model):
-    id = db.Column(db.String(200), primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    keywords = db.Column(db.Text, nullable=False, default='Test')
-    restaurant_visits = db.relationship('RestaurantVisit', backref='restaurant', lazy=True)
-    # price, so on (add other RICH features later)
+    # Restaurant model representing restaurant data
+    id = db.Column(db.String(200), primary_key=True)  # Restaurant ID, primary key
+    name = db.Column(db.String(100), nullable=False)  # Restaurant name
+    address = db.Column(db.String(100), nullable=False)  # Restaurant address
+    keywords = db.Column(db.Text, nullable=False, default='Test')  # Keywords associated with the restaurant
+    restaurant_visits = db.relationship('RestaurantVisit', backref='restaurant', lazy=True)  # Restaurant's visits
+    # Add other rich information after
 
     def __repr__(self):
+        # String representation of the Restaurant object
         return f"Restaurant('{self.name}', '{self.address}')"
-    
+
 class RestaurantVisit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    restaurant_id = db.Column(db.String(200), db.ForeignKey('restaurant.id'), nullable=False)
-    date_visited = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    rating = db.Column(db.Integer, nullable=False)
-    
+    # RestaurantVisit model representing user visits to restaurants
+    id = db.Column(db.Integer, primary_key=True)  # Visit ID, primary key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # User ID associated with the visit
+    restaurant_id = db.Column(db.String(200), db.ForeignKey('restaurant.id'), nullable=False)  # Restaurant ID associated with the visit
+    date_visited = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Date and time of the visit
+    rating = db.Column(db.Integer, nullable=False)  # Rating given by the user for the restaurant visit
+
     def __repr__(self):
+        # String representation of the RestaurantVisit object
         return f"Restaurant Visit('{self.user_id}', '{self.date_visited}', '{self.rating}')"
-    
